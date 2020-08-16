df6b36dcbc64fcd0500c49a03911fa56b3cec060
Kabila Haile Soboka <kabilahaile@gmail.com>
2020-08-16 12:58:32 +0300

    Google Search Done. Working on Result Click

--- src/selenium_test/GoogleSearch.java
+++ src/selenium_test/GoogleSearch.java
@@ -5,7 +5,11 @@
  */
 package selenium_test;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
@@ -18,7 +22,24 @@ public class GoogleSearch {
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get("https://www.google.com");
-        Thread.sleep(1000);
+        
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Software Automated Testing");
+        searchBox.submit();
+//        WebElement searchButton = driver.findElement(By.name("btnK"));
+//        searchButton.click();
+
+        List<WebElement> results =  driver.findElements(By.className("g"));
+        if(results.size() > 0){
+            WebElement firstResult = results.get(0);
+            WebElement resultOneLink = firstResult.findElement(By.tagName("a"));
+            resultOneLink.click();
+            Thread.sleep(5000);
+        }else{
+            
+        }
         
         
     }
